c368452f80a69d88bb1d5425092a308992eb8596
BrysonSaclausa <brys1saclaus2@gmail.com>
2020-09-18 21:57:35 -0700

    created project

---
+++ RandomUsersRedo/RandomUsersRedo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUsersRedo
+//
+//  Created by BrysonSaclausa on 9/18/20.
+//  Copyright © 2020 B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
